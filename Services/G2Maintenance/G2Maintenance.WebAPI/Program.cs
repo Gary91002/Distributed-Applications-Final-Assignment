9617007691bfeb3982f3f62ab4bba1cb746e39ca
Jacob-M-M <jacobbusiness04@outlook.com>
2026-04-21 17:06:32 -0400

    Merged Chris's Code

--- Services/G2Maintenance/G2Maintenance.WebAPI/Program.cs
+++ Services/G2Maintenance/G2Maintenance.WebAPI/Program.cs
@@ -4,11 +4,54 @@ using G2Maintenance.Application.Services;
 using G2Maintenance.Infrastructure.Data;
 using G2Maintenance.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var usageCounts = new Dictionary<string, int>();
 builder.Services.AddSingleton(usageCounts);
+
+var serviceName = "G2Maintenance";
+var serviceVersion = "1.0.0";
+
+builder.Logging.ClearProviders();
+builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
+builder.Logging.AddConsole();
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(
+        ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: serviceVersion));
+    options.IncludeFormattedMessage = true;
+    options.IncludeScopes = true;
+    options.ParseStateValues = true;
+    options.AddConsoleExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation(options =>
+            {
+                options.RecordException = true;
+                options.Filter = httpContext =>
+                    !httpContext.Request.Path.StartsWithSegments("/swagger") &&
+                    !httpContext.Request.Path.StartsWithSegments("/favicon.ico");
+            })
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter();
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter();
+    });
 
 builder.Services.AddDbContext<G2MaintenanceDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
